76077158125cee4c0576c6b93abeee8a56582eed
Spencer Dixon <spencercdixon@gmail.com>
2021-12-03 19:14:53 -0500

    Add hacky solution to D3P2

--- src/day3.cpp
+++ src/day3.cpp
@@ -30,18 +30,67 @@ int part_one(char *buf)
         epsilon |= (counts[i] > column_count ? 0 : 1) << (counts.size() - i - 1);
     }
 
-    printf("\ngamma: %d\n", gamma);
-    printf("epsilon: %d\n", epsilon);
     return gamma * epsilon;
+}
+
+u16 to_u16(String const& value)
+{
+    ASSERT(value.length() <= 16);
+    u16 result { 0 };
+    for (size_t i = 0; i < value.length(); ++i) {
+        result |= (value.characters()[i] == '1' ? 1 : 0) << (16 - (16 - value.length()) - i - 1);
+    }
+    return result;
+}
+
+String find_rating(Vec<String> lines, size_t index, bool for_oxygen)
+{
+    ASSERT(!lines.is_empty());
+    if (lines.size() == 1)
+        return lines.first();
+
+    Vec<size_t> counts = Vec<size_t>::with_capacity(lines[0].length(), 0);
+    for (auto &line : lines) {
+        for (size_t i = 0; i < line.length(); ++i) {
+            if (line.characters()[i] == '1') {
+                counts[i]++;
+            }
+        }
+    }
+
+    Vec<String> next;
+    bool has_more_ones = counts[index] >= (lines.size() / 2);
+    if (for_oxygen) {
+        for (auto& line : lines) {
+            if (has_more_ones && line.characters()[index] == '1') {
+                next.append(line);
+            } else if (!has_more_ones && line.characters()[index] == '0') {
+                next.append(line);
+            }
+        }
+    } else {
+        for (auto& line : lines) {
+            if (has_more_ones && line.characters()[index] == '0') {
+                next.append(line);
+            } else if (!has_more_ones && line.characters()[index] == '1') {
+                next.append(line);
+            }
+        }
+    }
+
+    return find_rating(next, ++index, for_oxygen);
 }
 
 int part_two(char *buf)
 {
-    return 0;
+    Vec<String> lines = String(buf).split('\n');
+    auto oxygen = find_rating(lines, 0, true);
+    auto c02_scrubber = find_rating(lines, 0, false);
+    return to_u16(oxygen) * to_u16(c02_scrubber);
 }
 
 // part-one: 3633500
-// part-two: 0
+// part-two: 4550283
 int main(int argc, char *argv[])
 {
     bool should_run_part_two = false;
